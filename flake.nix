b9ce37be106dcf7ba6fabd2364b2c854a7068e68
Knarkzel <knarkzel@gmail.com>
2023-08-28 22:11:58 +0200

    Just naersk

--- flake.nix
+++ flake.nix
@@ -6,25 +6,13 @@
       url = "github:nix-community/naersk";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    rust-overlay = {
-      url = "github:oxalica/rust-overlay";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.flake-utils.follows = "flake-utils";
-    };
   };
 
-  outputs = { self, nixpkgs, flake-utils, naersk-package, rust-overlay, ... }:
+  outputs = { self, nixpkgs, flake-utils, naersk-package, ... }:
     flake-utils.lib.eachDefaultSystem (system: let
-      overlays = [(import rust-overlay)];
-      pkgs = import nixpkgs {inherit system overlays;};
+      pkgs = import nixpkgs {inherit system;};
       naersk = pkgs.callPackage naersk-package {};
     in {
-      devShell = pkgs.mkShell {
-        buildInputs = [
-          pkgs.rust-bin.nightly.latest.default
-        ];
-      }; 
-
       defaultPackage = naersk.buildPackage {
         src = ./.;
       };
